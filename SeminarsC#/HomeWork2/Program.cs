881cfeb912486150d71fe1e244bcf8ec932b88ae
Aleksey Fursenko <phursenko@hotmail.com>
2022-06-09 21:17:02 +0300

    Home Work 2 completed

---
+++ SeminarsC#/HomeWork2/Program.cs
@@ -0,0 +1,101 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+/*
+Console.Clear();
+// string method
+string SecDigit(int num)
+{
+    if (num<100 ^ num>999) 
+    {
+        string result = "Your number isn't met above mentioned requirement!";
+        return result;
+    }
+    else
+    {
+        string result = (num + " -> " + num%100/10);
+        return result;
+    }
+}
+
+Console.Write("Please, input a number between 100 and 999: ");
+int num1  = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(SecDigit(num1));
+*/
+
+/*
+//int metod
+
+int SecDigit(int num)
+{
+    if (num<100 ^ num>999) 
+    {
+        int result = -1;
+        return result;
+    }
+    else
+    {
+        int result = num%100/10;
+        return result;
+    }
+}
+
+Console.Write("Please, input a number between 100 and 999: ");
+int num1  = Convert.ToInt32(Console.ReadLine());
+
+if (SecDigit(num1)<0) Console.WriteLine("Your number isn't met above mentioned requirement!");
+else Console.WriteLine(num1 + " -> " + SecDigit(num1));
+*/
+
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+/*
+Console.Clear();
+
+string ThirdDigit(string num)
+{
+    if (num.Length < 3) 
+    {
+        string result = "Your number isn't met above mentioned requirement!";
+        return result;
+    }
+    else
+    {
+        string result = (num + " -> " + num[2]);
+        return result;
+    }
+}
+
+Console.Write("Please, input a number more or equal 100: ");
+string num1  = Console.ReadLine();
+
+Console.WriteLine(ThirdDigit(num1));
+*/
+//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+/*
+Console.Clear();
+
+bool WeekEnd(int num)
+{
+    if(num < 6) return false;
+    else return true;
+}
+
+Console.WriteLine("Please, input a number matched to the weekday, as follow:" + "\n" +
+                " 1 - Monday" + "\n" +
+                " 2 - Tuesday" + "\n" +
+                " 3 - Wednesday" + "\n" +
+                " 4 - Thursday" + "\n" +
+                " 5 - Friday" + "\n" +
+                " 6 - Saturday" + "\n" +
+                " 7 - Sunday");
+int num1  = Convert.ToInt32(Console.ReadLine());
+
+if (num1 < 1 ^ num1 > 7)
+{
+    Console.WriteLine("Your number isn't met above mentioned requirement!");
+}
+else
+{
+    if(WeekEnd(num1)) Console.WriteLine(num1 + " is Weekend");
+    else Console.WriteLine(num1 + " is not Weekend");
+}
+*/
