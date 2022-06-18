dd4276bfeb010d3ea083d1e16a45e09957586f60
Phursenko <phursenko@hotmail.com>
2022-06-18 13:30:29 +0300

    Add Seminar4

---
+++ SeminarsC#/Seminar4/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+int SumOfDigits(int num)
+{
+    int sum = 0;
+
+    while(num > 0)
+    {
+        sum = sum + num % 10; // sum += num % 10;
+        num = num / 10;       // num /= 10;
+    }
+
+    return sum;
+}
+
+Console.WriteLine(SumOfDigits(12345));
+*/
+
+int size = 9;
+int[] array = new int[size];
+
+for(int i = 0; i < size; i++)
+{
+    array [i] = new Random().Next(0,2);
+    Console.Write(array[i] + "  ");
+}
